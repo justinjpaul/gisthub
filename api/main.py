54dd2f73b52347dd32ee413af487b278eff03648
jmaff <jmaff2003@gmail.com>
2023-11-18 14:31:34 -0500

    Successful Cloud Run deploy

--- api/main.py
+++ api/main.py
@@ -1,9 +1,16 @@
 import os
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, g, request
+import config
+import json
+import time
+import uuid
+
+from blueprints.activities import activities
 
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(activities, url_prefix="/api/v1/activities")
 
     # Error 404 handler
     @app.errorhandler(404)
@@ -20,9 +27,36 @@ def create_app():
     def custom_401(error):
         return Response("API Key required.", 401)
 
+    @app.route("/version", methods=["GET"], strict_slashes=False)
+    def version():
+        response_body = {
+            "success": 1,
+        }
+        return jsonify(response_body)
+
     @app.route("/ping")
     def hello_world():
         return "pong"
+
+    @app.before_request
+    def before_request_func():
+        execution_id = uuid.uuid4()
+        g.start_time = time.time()
+        g.execution_id = execution_id
+
+        print(g.execution_id, "ROUTE CALLED ", request.url)
+
+    @app.after_request
+    def after_request(response):
+        if response and response.get_json():
+            data = response.get_json()
+
+            data["time_request"] = int(time.time())
+            data["version"] = config.VERSION
+
+            response.set_data(json.dumps(data))
+
+        return response
 
     return app
 
